31cd5a97e519506be99d827f5ab354e39acdd7fc
pdr721 <pdr721@gmail.com>
2022-02-27 22:10:09 -0500

    fixed settings.py

--- healm/settings.py
+++ healm/settings.py
@@ -79,6 +79,8 @@ WSGI_APPLICATION = 'healm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+
 if DEVELOPMENT_MODE is True:
     DATABASES = {
         'default': {
